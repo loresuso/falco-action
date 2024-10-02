2434b96bcb0006c2f88f39b9e69fd0beeb76e3a3
Lorenzo Susini <susinilorenzo1@gmail.com>
2024-10-02 10:44:29 +0000

    fix: sysdig script should be now able to write to file
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

--- .sysdig/run_sysdig.sh
+++ .sysdig/run_sysdig.sh
@@ -5,11 +5,17 @@ filter="$2"
 output_fields="$3"
 output_file="$4"
 
-docker run --rm -d --privileged \
+if [ -z "$file_name" ] || [ -z "$filter" ] || [ -z "$output_fields" ] || [ -z "$output_file" ]; then
+    echo "Usage: $0 <file_name> <filter> <output_fields> <output_file>"
+    exit 1
+fi
+
+docker run --rm --privileged \
       -v /var/run/docker.sock:/host/var/run/docker.sock \
       -v /dev:/host/dev -v /proc:/host/proc:ro \
       -v /boot:/host/boot:ro \
       -v /lib/modules:/host/lib/modules:ro \
       -v /usr:/host/usr:ro \
       -v /tmp:/tmp \
-      --net=host sysdig/sysdig:latest sysdig --modern-bpf -r "$file_name" "$filter" -j -p $output_fields >> $output_file
+      --entrypoint /bin/bash \
+      --net=host sysdig/sysdig:latest -c "sysdig --modern-bpf -r \"$file_name\" \"$filter\" -j -p $output_fields >> $output_file"
