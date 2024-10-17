eed4de52b6e0ad95f97201ca744da82692d6d97c
Lorenzo Susini <susinilorenzo1@gmail.com>
2024-10-17 09:56:08 +0000

    update: delete hardcoded .sysdig and randomize pr branch name
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

--- stop/src/push_profile.sh
+++ stop/src/push_profile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -o pipefail
 
-#token="$2"
+profile_dir="$1"
+
 pr_message="[Falco](https://falco.org) has detected a deviation from normal behavior of the workflow.
 Review the changes in this PR and accept it in order to establish a new baseline."
 
@@ -9,9 +10,10 @@ git config --global user.email "oss@falco.com"
 git config --global user.name "Falco action"
 git fetch --all
 
-git checkout -B 'sysdig-profile-update' "origin/sysdig-profile-update" || git checkout -B 'sysdig-profile-update' "origin/$(git remote show origin | grep 'HEAD branch' | cut -d' ' -f5)"
+random_branch_name=falco-profile-update-$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 32 | xargs)
+git checkout -B "$random_branch_name" "origin/$random_branch_name" || git checkout -B "$random_branch_name" "origin/$(git remote show origin | grep 'HEAD branch' | cut -d' ' -f5)"
 
-git add $GITHUB_WORKSPACE/.sysdig/ 
-git commit -m 'update sysdig profile'
-git push -f --set-upstream origin sysdig-profile-update
+git add $GITHUB_WORKSPACE/$profile_dir/ 
+git commit -m 'update falco profile'
+git push -f --set-upstream origin $random_branch_name
 gh pr create --title "Updates to falco profile" --body "$(echo -e "$pr_message\n\nchanges:\n\`\`\`\nUpdates to falco profile\n\`\`\`")"
